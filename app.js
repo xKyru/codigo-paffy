d09a7f5ceb17b89878115c5a54122eb80736d7b2
xKyru <57547654+xKyru@users.noreply.github.com>
2024-02-26 22:07:50 -0600

    Updated

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ window.addEventListener("load", () => {
 
     let tlBg = gsap.timeline();
     tlBg.to(".text-wrapper", { backgroundPositionY: "50%", backgroundPositionX: "100%", backgroundSize: "200%", yoyo: true, duration: 30, repeat: -1, repeatDelay: 5, ease: "none" });
-    let tl = gsap.timeline();
-    tl.to(".letter", { rotateY: 360, stagger: 0.1, yoyo: false, repeat: -1, repeatDelay: 15 })
+
+    let tlLetter = gsap.timeline();
+    tlLetter.to(".letter", { rotateY: 360, stagger: 0.1, yoyo: false, repeat: -1, repeatDelay: 15 })
+
+    // let tlRemi = gsap.timeline();
+    // tlRemi.to(".remi", {scale: 1.05, duration: 1, yoyo: true, repeat: -1});
 });
