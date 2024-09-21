d99095ad6960cfd2286b3947aa043f7c47881784
xKyru <57547654+xKyru@users.noreply.github.com>
2024-09-21 13:28:12 -0600

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
 
-    const totalImages = 37;
+    const totalImages = 38;
     function randomNumber(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -9,18 +9,20 @@ window.addEventListener("load", () => {
 
     const conmeticUrl = `./img/cosmetics/cosmetic-${randomNumber(1, totalImages)}.png`;
     const cosmeticImg = document.querySelector("#cosmetic");
-    cosmeticImg.src = conmeticUrl;
+    // cosmeticImg.src = conmeticUrl;
 
     //GSAP
     var textWrapper = document.querySelector('.code .letters');
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
     let tlBg = gsap.timeline();
-    tlBg.to(".text-wrapper", { backgroundPositionY: "50%", backgroundPositionX: "100%", backgroundSize: "200%", yoyo: true, duration: 30, repeat: -1, repeatDelay: 5, ease: "none" });
+    tlBg.to(".text-wrapper", { backgroundPositionY: "10%", backgroundPositionX: "90%", backgroundSize: "200%", yoyo: true, duration: 60, repeat: -1, repeatDelay: 5, ease: "none" });
 
     let tlLetter = gsap.timeline();
-    tlLetter.to(".letter", { rotateY: 360, stagger: 0.1, yoyo: false, repeat: -1, repeatDelay: 15 })
-
+    // tlLetter.to(".letter", { rotateY: 360, stagger: 0.1, yoyo: false, repeat: -1, repeatDelay: 15 })
+    tlLetter.to(".letter", { textShadow: "0px 0px 20px #110039, 0px 0px 20px #D235DB", stagger: 0.1, yoyo: true, repeat: -1, repeatDelay: 0, duration: 2  })
+    // tlLetter.to(".letter", { textShadow: "0px 0px 15px #003648, 0px 0px 15px rgba(88, 234, 255, 0.8)", stagger: 0.1, yoyo: true, repeat: -1, repeatDelay: 0.2, duration: 2  })
+    // tlLetter.to(".letter", { textShadow: "0px 0px 15px #18331C, 0px 0px 15px rgba(122, 253, 139, 0.6)", stagger: 0.1, yoyo: true, repeat: -1, repeatDelay: 0.2, duration: 2  })
     // let tlRemi = gsap.timeline();
     // tlRemi.to(".remi", {scale: 1.05, duration: 1, yoyo: true, repeat: -1});
 });
