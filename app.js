8fba4bb3020867d69c989b2da1422030fafeec94
xKyru <57547654+xKyru@users.noreply.github.com>
2024-12-01 04:49:55 -0600

    new code

--- app.js
+++ app.js
@@ -15,8 +15,19 @@ window.addEventListener("load", () => {
     var textWrapper = document.querySelector('.code .letters');
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    let tlBg = gsap.timeline();
-    tlBg.to(".text-wrapper", { backgroundPositionY: "80%", backgroundPositionX: "95%", backgroundSize: "150%", yoyo: true, duration: 60, repeat: -1, repeatDelay: 5, ease: "none" });
+    // let tlBg = gsap.timeline();
+    // tlBg.to(".text-wrapper", { backgroundPositionY: "80%", backgroundPositionX: "95%", backgroundSize: "150%", yoyo: true, duration: 60, repeat: -1, repeatDelay: 5, ease: "none" });
+    
+    let tlCloud1 = gsap.timeline();
+    tlCloud1.to(".cloud-1", { x: -30, y: "50%", yoyo: true, duration: 20, repeat: -1, repeatDelay: 4, ease: "none" });
+    let tlCloud2 = gsap.timeline();
+    tlCloud2.to(".cloud-2", { x: 150, y: "35%", yoyo: true, duration: 22, repeat: -1, repeatDelay: 3, ease: "none" });
+    let tlCloud3 = gsap.timeline();
+    tlCloud3.to(".cloud-3", { x: 200, y: "25%", yoyo: true, duration: 25, repeat: -1, repeatDelay: 4, ease: "none" });
+    let tlCloud4 = gsap.timeline();
+    tlCloud4.to(".cloud-4", { x: -180, y: "15%", yoyo: true, duration: 21, repeat: -1, repeatDelay: 5, ease: "none" });
+    let tlCloud5 = gsap.timeline();
+    tlCloud5.to(".cloud-5", { x: -30, y: "30%", yoyo: true, duration: 23, repeat: -1, repeatDelay: 3, ease: "none" });
 
     let tlLetter = gsap.timeline();
     tlLetter.to(".letter", { scale:1.1, repeat: -1, stagger:0.1, repeatDelay: 5, yoyo: true, ease: "bounce.out" })
@@ -28,9 +39,13 @@ window.addEventListener("load", () => {
     // tlRemi.to(".remi", {scale: 1.05, duration: 1, yoyo: true, repeat: -1});
 
     // Image fade
-    let img1 = gsap.timeline();
-    img1.to(".code .img1", {opacity: 0, repeat: -1, repeatDelay: 300, delay: 300, yoyo: true});
+    // let img1 = gsap.timeline();
+    // img1.to(".code .img1", {opacity: 0, repeat: -1, repeatDelay: 300, delay: 300, yoyo: true});
 
-    let img2 = gsap.timeline();
-    img2.to(".code .img2", {opacity: 1, repeat: -1, repeatDelay: 300, delay: 300, yoyo: true});
+    // let img2 = gsap.timeline();
+    // img2.to(".code .img2", {opacity: 1, repeat: -1, repeatDelay: 300, delay: 300, yoyo: true});
+
+    particlesJS.load('particles-js', './particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 });
